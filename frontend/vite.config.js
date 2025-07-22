54995ef5077fec53753ca21c3a526abae90af168
Dhruv Kumar <kdhruv7505@gmail.com>
2025-07-22 18:41:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4001,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://bubble-backend-a2x9.onrender.com",
         changeOrigin: true,
       },
     },
